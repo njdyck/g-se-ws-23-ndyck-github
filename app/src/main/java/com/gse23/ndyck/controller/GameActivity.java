1bffaab5139b78e52521b4b662ef99bfd1bececc
Noah Dyck <ndyck@techfak.de>
2023-11-22 10:52:38 +0100

    refs#67388 added 1 button and 2 Textfields for the game.

--- app/src/main/java/com/gse23/ndyck/controller/GameActivity.java
+++ app/src/main/java/com/gse23/ndyck/controller/GameActivity.java
@@ -3,8 +3,10 @@ package com.gse23.ndyck.controller;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.gse23.ndyck.R;
 import com.gse23.ndyck.view.Image;
 
@@ -17,6 +19,8 @@ public class GameActivity extends MainActivity {
     HashMap<String, List<String>> names = new HashMap<String,List<String>>();
     String map;
     private List<String> displayedImages = new ArrayList<>();
+    EditText inputLon;
+    EditText inputLat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,9 @@ public class GameActivity extends MainActivity {
         map = getIntent().getStringExtra("Ausgewählte Map: ");
         Log.i("Ausgewählte Map: ", map);
 
+        inputLat = (EditText) findViewById(R.id.inputLat);
+        inputLon = (EditText) findViewById(R.id.inputLon);
+
         names = Image.getPicInfo(this, map);
         Image.logFileInfos(names);
         Image.displayRandomImage(this, map, imageView, names, displayedImages);
@@ -34,5 +41,19 @@ public class GameActivity extends MainActivity {
 
     public void nextPic(View view) {
         Image.displayRandomImage(this, map, imageView, names, displayedImages);
+        inputLon.setText("");
+        inputLat.setText("");
+    }
+    public void guess(View view) {
+        double lat = Double.parseDouble(String.valueOf(inputLat.getText()));
+        double lon = Double.parseDouble(String.valueOf(inputLon.getText()));
+
+        if (lon < -180 || lon > 180) {
+            Snackbar.make(view, "Der Wertebereich für Längengerade liegt bei: {-180,180}",
+                    Snackbar.LENGTH_LONG).show();
+        } if (lat < -90 || lat > 90) {
+            Snackbar.make(view, "Der Wertebereich für Breitengrade liegt bei: {-90,90}",
+                    Snackbar.LENGTH_LONG).show();
+        }
     }
 }
